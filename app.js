7c5379baa60d6f8a794f612ca5f4349611acf792
Piyush <piyush_sagar@persistent.co.in>
2016-08-12 11:59:33 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+var Operations = require(process.cwd() + '/src/add.js');
+
+
+
+var ops = new Operations();
+
+
+console.log(" --------- : "+ ops.add(5,12));
---
+++ src/add.js
@@ -0,0 +1,13 @@
+
+
+function Operations(){
+
+}
+
+
+module.exports = Operations ;
+
+Operations.prototype.add = function(a,b){
+
+    return a+b ;
+}
