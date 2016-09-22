fd73aca29ed069303c3763c03b9d506d022157b4
Piyush <piyush_sagar@persistent.co.in>
2016-09-22 10:48:20 +0530

    Starts build on Travis-CI

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ var Operations = require(process.cwd() + '/src/add.js');
 var ops = new Operations();
 
 
-console.log(" ---------  : "+ ops.add(5,12));
+console.log(" --------- : "+ ops.add(5,12));
 
 //7e509175ada66ad61af7d75eb2680c7390e4f1bb
